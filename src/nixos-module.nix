6ac4bfe1edcabac2612c81860ddd7a58360081ca
Benjamin Levy <7348004+io12@users.noreply.github.com>
2023-11-24 23:00:14 -0500

    Add snap to AppArmor include path

--- src/nixos-module.nix
+++ src/nixos-module.nix
@@ -30,7 +30,10 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    security.apparmor.enable = true;
+    security.apparmor = {
+      enable = true;
+      packages = [ snap ];
+    };
 
     environment.systemPackages = [ snap ];
 
